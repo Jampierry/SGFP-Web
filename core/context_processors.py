6b5b748ab1c0f0d39374feecf4c3b674d23ef1c8
Jampierry Ferreira Tome da Silva <jampierry.silva@dipaula.com.br>
2025-07-15 14:44:52 -0300

    Atualiza projeto com a melhoria da função icones coloridos do menu lateral, documentação e requirements com as mudanças do dia

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,25 +1,13 @@
 from core.models import Configuracao
 
-def tema_config(request):
-    """Context processor para disponibilizar configurações do tema em todas as páginas"""
+def preferencias_usuario(request):
     if request.user.is_authenticated:
-        try:
-            configuracao = request.user.configuracao
-        except Configuracao.DoesNotExist:
-            configuracao = Configuracao.objects.create(usuario=request.user)
-            return {
-            'configuracao': configuracao,
-            'icones_coloridos': configuracao.icones_coloridos
-            }
+        configuracao, _ = Configuracao.objects.get_or_create(usuario=request.user)
+        return {
+            'icones_coloridos': configuracao.icones_coloridos,
+            'tema_usuario': configuracao.dashboard_tema,
+        }
     return {
-        'configuracao': None,
-        'icones_coloridos': False
-    }
-
-def tema_usuario(request):
-    if request.user.is_authenticated:
-        try:
-            return {'tema_usuario': request.user.configuracao.dashboard_tema}
-        except Exception:
-            return {'tema_usuario': 'corona-dark'}
-    return {'tema_usuario': None} 
+        'icones_coloridos': False,
+        'tema_usuario': 'corona-dark',
+    } 
